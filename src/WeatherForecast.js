c4e1469a020b4652c0a0c53b7587425d4c54127c
Genevieve <genevievehines@gmail.com>
2021-10-21 21:50:05 +1100

    update forecast display

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,15 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <hr className="mt-5 mb-4" />
         <div className="row">
-          <div className="col-2">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
